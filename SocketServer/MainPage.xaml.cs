e5c76624c067f933822ec50bd7db3fd0a7e20211
Huy (Harold) Q. BUI <harold@enclave.vn>
2017-07-19 09:14:09 +0700

    first commit

---
+++ SocketServer/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SocketServer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+      
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Create a StreamSocketListener to start listening for TCP connections.
+                StreamSocketListener socketListener = new StreamSocketListener();
+
+                //Hook up an event handler to call when connections are received.
+                socketListener.ConnectionReceived += SocketListener_ConnectionReceived;
+
+                //Start listening for incoming TCP connections on the specified port. You can specify any port that's not currently in use.
+                await socketListener.BindServiceNameAsync("7777", SocketProtectionLevel.PlainSocket);
+            }
+            catch (Exception ex)
+            {
+                //Handle exception.
+            }
+        }
+
+        private async void SocketListener_ConnectionReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            //Read line from the remote client.
+            Stream inStream = args.Socket.InputStream.AsStreamForRead();
+            StreamReader reader = new StreamReader(inStream);
+            var response = await reader.ReadLineAsync();
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+             {
+                 text.Text = response;
+             });
+            //Send the line back to the remote client.
+            Stream outStream = args.Socket.OutputStream.AsStreamForWrite();
+            StreamWriter writer = new StreamWriter(outStream);
+            await writer.WriteLineAsync(response);
+            await writer.FlushAsync();
+        }
+    }
+}
